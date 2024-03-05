4d435fae832626b0d8325c780386763df89ec961
liurunxuan <2641205514@qq.com>
2024-03-06 00:21:38 +0800

    不用修改路径了

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 from ultralytics import YOLO
+import os
+
+current_path = os.path.dirname(__file__)
 
 # Load a model
 model = YOLO('yolov8n.yaml')  # build a new model from YAML
@@ -6,4 +9,4 @@ model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(task="detect", data='/home/lrx/project/boot8/mydata/VOC12BootTT.yaml', epochs=100, imgsz=640)
+results = model.train(task="detect", data=f'{current_path}/mydata/VOC12BootTT.yaml', epochs=1, imgsz=640)
