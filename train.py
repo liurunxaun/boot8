79e624af0f3ce41471f3ff586930f95fb0580127
liurunxuan <2641205514@qq.com>
2024-03-08 22:41:57 +0800

    训练结果

--- train.py
+++ train.py
@@ -9,4 +9,4 @@ model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(task="detect", data=f'{current_path}/mydata/VOC12BootTT.yaml', epochs=1, imgsz=640)
+results = model.train(task="detect", data=f'{current_path}/mydata/VOC12BootTT.yaml', epochs=50, imgsz=640)
